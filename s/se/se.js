53c3f408bcf6ec11f906511f4b2cdaf261b7d4ed
Chris Cummins <chrisc.101@gmail.com>
2014-12-08 20:23:53 +0000

    se: Add WebGL space visualisation
    This patch adds a terrain generator which is used to create simulated
    "spaces", using three.js. It is based on the webgl terrain demo. See:
    
        http://threejs.org/examples/webgl_geometry_terrain.html

--- s/se/se.js
+++ s/se/se.js
@@ -7,6 +7,10 @@
  * Source code: https://github.com/ChrisCummins/chriscummins.github.io
  * http://chriscummins.cc
  *
+ *
+ * Space generator based on webgl terrain demo. See:
+ *
+ *    http://threejs.org/examples/webgl_geometry_terrain.html
  */
 
 var SpaceExplorer = function() {
@@ -38,8 +42,13 @@ var SpaceExplorer = function() {
       frequency: {
         slider: $('#frequency-slider'),
         label: $('#frequency')
+      },
+      size: {
+        slider: $('#size-slider'),
+        label: $('#size')
       }
-    }
+    },
+    space: $('#space')
   };
 
 
@@ -123,7 +132,13 @@ var SpaceExplorer = function() {
   };
 
   // INITIALISATION.
-  
+
+  // WebGL browser compatability check.
+  if (!Detector.webgl) {
+    gui.space[0].innerHTML = "";
+    Detector.addGetWebGLMessage();
+  }
+
   // Our simulation instance.
   var simulation = new Simulation();
 
@@ -136,7 +151,7 @@ var SpaceExplorer = function() {
       simulation.pause();
     else
       simulation.run();
-  }); 
+  });
   // "Step" button event handler.
   gui.btn.step.click(function() { simulation.step(); });
 
@@ -155,10 +170,215 @@ var SpaceExplorer = function() {
       simulation.frequency = ui.value;
     }
   });
-  
+
+  // Size slider initialise.
+  gui.ctrl.size.slider.slider({
+    range: 'min',
+    min: 16,
+    max: 512,
+    step: 16,
+    value: parseInt(gui.ctrl.size.label.text()),
+    slide: function(event, ui) {
+      gui.ctrl.size.label.text(ui.value + ' x ' + ui.value);
+      worldSize = ui.value;
+      generateTerrain();
+    }
+  });
+
   // Enable buttons.
   enableBtn(gui.btn.step);
   enableBtn(gui.btn.run);
+
+
+  // Update WebGL space rendering.
+  var onWindowResize = function() {
+    var width = gui.space.width();
+    var height = gui.space.height();
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+    controls.handleResize();
+  };
+
+
+  // Generate terrain height map.
+  var createData = function(size) {
+    var area = size * size;
+    var data = new Uint8Array(area);
+    var noise = new ImprovedNoise().noise;
+    var quality = 1;
+    var z = Math.random() * 100;
+
+    for (var j = 0; j < 4; j ++) {
+      for (var i = 0; i < area; i ++) {
+	var x = i % size;
+        var y = ~~ (i / size);
+	data[i] += Math.abs(noise(x / quality, y / quality, z) * quality * 1.75);
+      }
+
+      quality *= 5;
+    }
+
+    return data;
+  };
+
+
+  // Generate terrain texture.
+  var generateTexture = function(data, width, height) {
+    var canvas;
+    var canvasScaled;
+    var context;
+    var image;
+    var imageData;
+    var level;
+    var diff;
+    var vector3 = new THREE.Vector3(0, 0, 0);
+    var ambience = .2;
+    var sun = new THREE.Vector3(ambience, ambience, ambience);
+    var shade;
+
+    canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    context = canvas.getContext('2d');
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, width, height);
+
+    image = context.getImageData(0, 0, canvas.width, canvas.height);
+    imageData = image.data;
+
+    for (var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++) {
+      vector3.x = data[j - 2] - data[j + 2];
+      vector3.y = 2;
+      vector3.z = data[j - width * 2] - data[j + width * 2];
+      vector3.normalize();
+
+      shade = vector3.dot(sun);
+
+      // R,G,B values:
+      imageData[i]     = (110 + shade * 128)  * (0.5 + data[j]  * 0.007);
+      imageData[i + 1] = (32  + shade * 96)   * (0.5 + data[j]  * 0.007);
+      imageData[i + 2] = (0   + shade * 50)   * (0.5 + data[j]  * 0.007);
+    }
+
+    context.putImageData(image, 0, 0);
+
+    // Scaled 4x
+
+    canvasScaled = document.createElement('canvas');
+    canvasScaled.width = width * 4;
+    canvasScaled.height = height * 4;
+
+    context = canvasScaled.getContext('2d');
+    context.scale(4, 4);
+    context.drawImage(canvas, 0, 0);
+
+    image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
+    imageData = image.data;
+
+    for (var i = 0, l = imageData.length; i < l; i += 4) {
+      var v = ~~ (Math.random() * 5);
+      imageData[i] += v;
+      imageData[i + 1] += v;
+      imageData[i + 2] += v;
+    }
+
+    context.putImageData(image, 0, 0);
+
+    return canvasScaled;
+  }
+
+
+  var generateTerrain = function() {
+    if (mesh)
+      scene.remove(mesh);
+
+    // Terrain data.
+    data = createData(worldSize);
+
+    // Terrain geometry.
+    var geometry = new THREE.PlaneBufferGeometry(7500, 7500,
+                                                 worldSize - 1,
+                                                 worldSize - 1);
+    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+    var vertices = geometry.attributes.position.array;
+
+    for (var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3)
+      vertices[j + 1] = data[i] * 10;
+
+    // Terain texture.
+    texture = new THREE.Texture(generateTexture(data, worldSize, worldSize),
+                                 new THREE.UVMapping(),
+                                 THREE.ClampToEdgeWrapping,
+                                 THREE.ClampToEdgeWrapping);
+    texture.needsUpdate = true;
+
+    // Terrain material.
+    var material = new THREE.MeshBasicMaterial({ map: texture });
+
+    // Terrain mesh.
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Update camera.
+    distanceFactor = 6144 / worldSize;
+    camera.position.x = -distanceFactor * worldSize;
+    camera.position.y = distanceFactor * worldSize;
+    camera.position.z = distanceFactor * worldSize;
+  };
+
+
+  // Animate optimisation space.
+  var animate = function() {
+    requestAnimationFrame(animate);
+    render();
+  }
+
+
+  // Render optimisation space.
+  var render = function() {
+    controls.update(clock.getDelta());
+    renderer.render(scene, camera);
+  }
+
+
+  var worldSize = 256;
+  var distanceFactor = 6144 / worldSize;
+  var bg = new THREE.Color(0xfefefe);
+
+  var camera = new THREE.PerspectiveCamera(45, gui.space.width()
+                                           / gui.space.height(), 1, 30000);
+  var clock = new THREE.Clock();
+  var container = gui.space[0];
+  var controls = new THREE.OrbitControls(camera);
+  var data;
+  var mesh;
+  var renderer;
+  var scene = new THREE.Scene();
+  var texture;
+
+
+  // Initialise WebGL optimisation space.
+  var init = function() {
+    var width = gui.space.width();
+    var height = gui.space.height();
+
+    generateTerrain();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(bg);
+    renderer.setSize(width, height);
+
+    container.innerHTML = "";
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+  }
+
+  init();
+  animate();
 };
 
 
