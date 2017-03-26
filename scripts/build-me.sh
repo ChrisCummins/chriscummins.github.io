70658e8f1ee12752a475f1e82ad07b3d24342eef
Chris Cummins <chrisc.101@gmail.com>
2017-03-26 10:54:03 +0100

    don't cd to top level dir

--- scripts/build-me.sh
+++ scripts/build-me.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 #
-# Slightly hacky bash script to quickly start of the development tools.
+# Start the development tools.
 #
-topsrcdir=chriscummins.cc
-
-# Look up the top source directory. Do this by traversing up the directory tree
-# from the current directory. If we haven't found the top directory by the time
-# we reach root (/), fail.
-get_source_directory() {
-	local root=""
-
-	while [[ "$(pwd)" != "/" ]]; do
-		if [[ "${PWD##*/}" = "$topsrcdir" ]]; then
-			echo "$(pwd)"
-			return
-		fi
-		cd ..
-	done
-
-	echo "fatal: Unable to locate source directory." >&2
-
-	# If we can't find a real path to change to, then return something bogus
-	# so that cd will fail and the script will terminate.
-	echo "/not/a/real/file/path"
-}
-
 set -e
-
-cd $(get_source_directory)
 
 # Spawn a jekyll server to host the website on localhost:4000.
 bundle exec jekyll serve &
