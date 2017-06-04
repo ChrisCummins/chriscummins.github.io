95b9af8298364cbc2c35e747fdb47a557409481b
Chris Cummins <chrisc.101@gmail.com>
2017-06-04 14:24:36 +0100

    scripts: Now less, use --drafts

--- scripts/build-me.sh
+++ scripts/build-me.sh
@@ -8,19 +8,13 @@ set -eux
 bundle exec jekyll serve &
 serve_pid=$!
 
-# Spawn a process to watch the css/ directory and automatically compile .less
-# files into compressed .css files.
-watch-less -c -d css -e .css &
-watch_less_pid=$!
-
 finish() {
         # tidy up by ensuring any forked processes terminate
         set +e
         kill $serve_pid 2>/dev/null
-        kill $watch_less_pid 2>/dev/null
 }
 trap finish EXIT
 
 # Spawn a process to watch the top directory and rebuild the website whenever
 # anything changes.
-bundle exec jekyll build --watch
+bundle exec jekyll build --watch --drafts
