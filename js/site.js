062bcfb7a2f1ad8e8410fd6b4a7403060464f6e9
Chris Cummins <chrisc.101@gmail.com>
2017-06-06 12:46:02 +0100

    jumbotron and col-right fixes

--- js/site.js
+++ js/site.js
@@ -50,6 +50,25 @@ $('.has-caret .collapse')
   });
 
 /*
+* Clamped-width.
+* Usage:
+*  <div data-clampedwidth=".myParent">This long content will force clamped width</div>
+*
+* Author: LV
+*/
+$('[data-clampedwidth]').each(function () {
+    var elem = $(this);
+    var parentPanel = elem.data('clampedwidth');
+    var resizeFn = function () {
+        var sideBarNavWidth = $(parentPanel).width() - parseInt(elem.css('paddingLeft')) - parseInt(elem.css('paddingRight')) - parseInt(elem.css('marginLeft')) - parseInt(elem.css('marginRight')) - parseInt(elem.css('borderLeftWidth')) - parseInt(elem.css('borderRightWidth'));
+        elem.css('width', sideBarNavWidth);
+    };
+
+    resizeFn();
+    $(window).resize(resizeFn);
+});
+
+/*
  * jquery.hypher.js -- A fast and small JavaScript hyphenation engine
  * Copyright 2011 Bram Stein.
  * BSD License.
